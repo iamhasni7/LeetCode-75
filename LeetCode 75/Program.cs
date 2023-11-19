5afd272061ce4ef00966ec72b7735a21a4ea8788
Hussnain <hasni.rock@yahoo.com>
2023-11-19 23:31:55 +0500

    LeetCode75 first challenge "1768. Merge Strings Alternately"

--- LeetCode 75/Program.cs
+++ LeetCode 75/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System.Text;
+
+namespace LeetCode75
+{
+    // 1768. Merge Strings Alternately
+    public class Solution
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int firstString = word1.Length;
+            int secondString = word2.Length;
+
+            int i = 0;
+            while (i < firstString || i < secondString) 
+            {
+                if (i < firstString)
+                {
+                    sb.Append(word1[i]);
+                }
+
+                if (i < secondString)
+                {
+                    sb.Append(word2[i]);
+                }
+                i++;
+            }
+
+            return sb.ToString();
+        }
+
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var result = solution.MergeAlternately("abc", "pqrst");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
